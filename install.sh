c8715d0193eb6f0c0dd451b6d6957a3644fb634b
tanvibattu <43849158+tanvibattu@users.noreply.github.com>
2021-02-13 17:20:07 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install -y python 3
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-django
+pip3 install djangorestframework
+python -m pip3 install django-cors-headers
+sudo apt-get install -y curl
+sudo apt-get install -y nodejs
+sudo npm install -y npm@latest -g
+npm i bootstrap
+npm i reactstrap react react-dom
+npm install axios
+npm install --save react-boostrap
+
