4dc3ab56c57aa2eb3bcfb16b9211407befca8ae5
tanvibattu <43849158+tanvibattu@users.noreply.github.com>
2021-02-14 00:38:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+sudo apt-get install -y curl
+cd backend
 sudo apt-get install -y python 3
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-django
 pip3 install djangorestframework
 python -m pip3 install django-cors-headers
-sudo apt-get install -y curl
+cd frontend
 sudo apt-get install -y nodejs
 sudo npm install -y npm@latest -g
 npm i bootstrap
