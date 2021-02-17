4b6b3ef76a05b9ee07cc85a9e88225688138583c
Tanvi Battu <tanvibattu00@gmail.com>
2021-02-17 17:24:36 +0530

    final code

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://127.0.0.1:8000/memes/";
-export const API_URL = "https://xmeme-tanvi-website.herokuapp.com/memes";
+export const API_URL = "https://xmeme-tanvi-website.herokuapp.com/memes/";
